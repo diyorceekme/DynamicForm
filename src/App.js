65386f7cafb69854d7de9a6ed68f92a3199ed0a7
Diyorceek <jumanazarovdiyorbek2007@gmail.com>
2022-08-18 19:15:03 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,176 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-expressions */
+// import { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [inputFields, setInputFields] = useState([
+//     {name: "", age: ""}
+//   ])
+
+//   const handleFormChange = (index, event) => {
+//     let data = [...inputFields]
+//     data[index][event.target.name] = event.target.value
+//     setInputFields(data)
+//   }
+
+//   const addFields = () => {
+//     let newfield = { name: "", age: "" }
+//     setInputFields([...inputFields, newfield])
+//   }
+
+//   const submit = (e) => {
+//     e.preventDefault();
+//     console.log(inputFields)
+//   }
+
+//   const removeFields = (index) => {
+//     let data = [...inputFields]
+//     data.splice(index, 1)
+//     setInputFields(data)
+//   }
+ 
+//   return (
+//     <div className="App">
+//       <form onSubmit={submit}>
+//         {inputFields.map((input, index) => {
+//           return (
+//             <div key={index}>
+//               <input name="name" placeholder='Name' value={input.name} onChange={event => handleFormChange(index, event)} />
+//               <input name="age" placeholder='Age' value={input.age} onChange={event => handleFormChange(index, event)} />
+//               <button onClick={() => removeFields(index)}>Remove</button>
+//             </div>
+//           )
+//         })}
+//         <div>
+//           <button onClick={addFields}>Add more...</button>
+//           <button onClick={submit}>Submit</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useFormik } from 'formik';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputFields, setInputFields] = useState([
+    {firstName: "", lastName: "", email: "", password: "", confirmPassword: ""}
+  ])
+  const formik = useFormik({
+    initialValues:{
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values))
+    }
+  })
+
+  const handleFormChange = (index, event) => {
+    let data = [...inputFields]
+    data[index][event.target.name] = event.target.value
+    setInputFields(data)
+  }
+  const addFields = () => {
+    let newfield = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    }
+    setInputFields([...inputFields, newfield])
+  }
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(inputFields)
+  }
+  const removeFields = (index) => {
+    let data = [...inputFields]
+    data.splice(index, 1)
+    setInputFields(data)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container d-flex flex-wrap'>
+      {inputFields.map((input, index) => (
+        <div className='form'>
+          <p>
+              <label htmlFor="firstName">First Name</label><br/>
+              <input
+                  className="input"
+                  type="text"
+                  name="firstName"
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName || input.firstName}
+                  onChange={event => handleFormChange(index, event)}
+              />
+          </p>
+          <p>
+              <label htmlFor="lastName">Last Name</label><br/>
+              <input
+                  className="input"
+                  type="text"
+                  name="lastName"
+                  onChange={event => handleFormChange(index, event)}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.lastName || input.lastName}
+              />
+          </p>
+          <p>
+              <label htmlFor="email">Email</label><br/>
+              <input
+                  className="input"
+                  type="email"
+                  name="email"
+                  onChange={event => handleFormChange(index, event)}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email || input.email}
+              />
+          </p>
+          <p>
+              <label htmlFor="password">Password</label><br/>
+              <input
+                  className="input"
+                  type="password"
+                  name="password"
+                  onChange={event => handleFormChange(index, event)}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password || input.password}
+              />
+          </p>
+          <p>
+              <label htmlFor="confirmPassword">Confirm Password</label><br/>
+              <input
+                  className="input"
+                  type="password"
+                  name="confirmPassword"
+                  onChange={event => handleFormChange(index, event)}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.confirmPassword || input.confirmPassword}
+              />
+          </p>
+          <button onClick={() => removeFields(index)} className="delete">Remove</button>
+        </div>
+      ))}
     </div>
+    <div className='container mb-5 mt-5'>
+      <button onClick={addFields} className="add">Add more...</button>
+      <button
+        onClick={submit}
+        type="submit"
+        className='submit'
+      >Submit</button>
+    </div>
+    </>
   );
 }
 
